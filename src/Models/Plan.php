0510265e0cfe8e9918e29d92f070900137245848
masterix21 <6555012+masterix21@users.noreply.github.com>
2025-02-24 10:54:20 +0000

    Fix styling

--- src/Models/Plan.php
+++ src/Models/Plan.php
@@ -5,7 +5,6 @@ namespace LucaLongo\Subscriptions\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
